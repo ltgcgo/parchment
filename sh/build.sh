25bfa9c8d5e390d6d457c95d4ddfbb939f9cdf7c
Lumière Élevé <88174309+PoneyClairDeLune@users.noreply.github.com>
2023-01-31 06:19:26 +0000

    Repo init.

---
+++ sh/build.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+mkdir -p build
+#mkdir -p proxy
+# Remove the dev files
+rm -rv build/*.js
+rm -rv build/*.map
+# Using esbuild to build all JS files
+#esbuild --bundle src/index.js --outfile=build/index.js --minify --sourcemap
+#esbuild --bundle src/index.js --target=es6 --outfile=build/index.es6.js --minify --sourcemap
+ls -1 src | while IFS= read -r dir ; do
+	if [ -e "src/${dir}/index.js" ] ; then
+		shx live $dir --minify > /dev/null
+	fi
+	if [ -e "src/${dir}/index.mjs" ] ; then
+		shx live $dir --minify > /dev/null
+	fi
+done
+#rm -rv proxy/*.map
+# Finalizing most builds
+#ls -1 src | while IFS= read -r dir ; do
+	#if [ -e "src/${dir}/prefixer.js" ] ; then
+		#cat src/${dir}/prefixer.js > build/${dir}.js
+	#fi
+	#if [ -e "proxy/${dir}.js" ] ; then
+		#cat proxy/${dir}.js >> build/${dir}.js
+	#fi
+#done
+# Node specific
+#mkdir -p proxy/node
+#mv build/node.js proxy/node/index.js
+#rm proxy/node.js
+exit
---
+++ sh/live.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#rm -rv build/${1:default}*
+inject=" "
+prepend=" "
+append=" "
+format="iife"
+ext="js"
+if [ -e "src/${1:-default}/prefix.js" ] ; then
+	inject="--inject:src/${1:-default}/prefix.js"
+fi
+if [ -e "src/${1:-default}/prefix.js" ] ; then
+	prepend="--prepend:src/${1:-default}/prefix.js"
+fi
+if [ -e "src/${1:-default}/affix.js" ] ; then
+	append="--append:src/${1:-default}/affix.js"
+fi
+if [ -e "src/${1:-default}/index.mjs" ] ; then
+	format="esm"
+	ext="mjs"
+fi
+esbuild --bundle src/${1:-default}/index.${ext} $prepend $append $inject --format=$format --charset=utf8 --outfile=build/${1:-default}.${ext} ${2:---minify-whitespace --minify-syntax --sourcemap --watch}
+#cat proxy/${1:-default}.js
+exit
